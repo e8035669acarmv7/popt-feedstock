7215e2985d674c240670bd7698ec961e55e95b29
Isuru Fernando <isuruf@gmail.com>
2020-11-28 13:10:17 -0600

    Fix updating config.sub

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,9 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 set -e -o pipefail
 
-# If platform detection fails, these should be updated in the meta.yaml from:
-# http://git.savannah.gnu.org/gitweb/?p=config.git&view=view+git+repository
-mv config-updated.guess config.guess
-mv config-updated.sub config.sub
-
 ./configure --prefix=$PREFIX
 make
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+  make check
 fi
 make install
